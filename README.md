b446b6a477958de80755bc5971da150face17401
adihadzhiev <adhadzhiev@tu-sofia.bg>
2023-03-26 17:22:09 +0300

    code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Network Tracking using Wireshark and Google Maps
- 
+Introduction
+This code is a Python script that extracts IP addresses from a pcap file, and plots their geo-locations on a KML file. It uses the dpkt module to read the pcap file, the socket module to convert IP addresses to human-readable format, and the pygeoip module to determine the geo-location of the IP addresses.
+
+Installation
+To use this code, you need to have Python installed on your machine, as well as the dpkt, socket, and pygeoip modules.
+
+You can install these modules using pip, by running the following command in your terminal:
+
+pip install dpkt socket pygeoip
+
+Usage
+To use this code, you need to have a pcap file that contains network traffic. You can run the code by running the main() function, which reads the pcap file, extracts the IP addresses, determines their geo-locations, and generates a KML file.
+
+To run the code, simply execute the following command in your terminal:
+
+python pcap-to-kml.py
+
+The code will output the KML file to the console, which you can then save to a file using output redirection, like this:
+
+python pcap-to-kml.py > output.kml
+
+This will save the KML output to a file named output.kml in your current directory.
---
+++ main.py
@@ -0,0 +1,60 @@
+import dpkt
+import socket
+import pygeoip
+
+gi = pygeoip.GeoIP('GeoLiteCity.dat')
+
+def retKML(dstip, srcip):
+    dst = gi.record_by_name(dstip)
+    src = gi.record_by_name('xx.xx.xx.xx') ## your ip adress
+    try:
+        dstlongitude = dst['longitude']
+        dstlatitude = dst['latitude']
+        srclongitude = src['longitude']
+        srclatitude = src['latitude']
+        kml = (
+            '<Placemark>\n'
+            '<name>%s</name>\n'
+            '<extrude>1</extrude>\n'
+            '<tessellate>1</tessellate>\n'
+            '<styleUrl>#transBluePoly</styleUrl>\n'
+            '<LineString>\n'
+            '<coordinates>%6f,%6f\n%6f,%6f</coordinates>\n'
+            '</LineString>\n'
+            '</Placemark>\n'
+        )%(dstip, dstlongitude, dstlatitude, srclongitude, srclatitude)
+        return kml
+    except:
+        return ''
+
+def plotIPs(pcap):
+    kmlPts = ''
+    for (ts, buf) in pcap:
+        try:
+            eth = dpkt.ethernet.Ethernet(buf)
+            ip = eth.data
+            src = socket.inet_ntoa(ip.src)
+            dst = socket.inet_ntoa(ip.dst)
+            KML = retKML(dst, src)
+            kmlPts = kmlPts + KML
+        except:
+            pass
+    return kmlPts
+
+def main():
+    f = open('test.pcap', 'rb')
+    pcap = dpkt.pcap.Reader(f)
+    kmlheader = '<?xml version="1.0" encoding="UTF-8"?> \n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n'\
+    '<Style id="transBluePoly">' \
+                '<LineStyle>' \
+                '<width>1.5</width>' \
+                '<color>501400E6</color>' \
+                '</LineStyle>' \
+                '</Style>'
+    
+    kmlfooter = '</Document>\n</kml>\n'
+    kmldoc=kmlheader+plotIPs(pcap)+kmlfooter
+    print(kmldoc)
+
+if __name__ == '__main__':
+    main()    
